807e11f062fc1bcfaec00385a474e195ce465711
‘Timothy <Timothy.viccari@gmail.com>
2021-04-20 20:20:30 -0400

    added error message

--- src/App.js
+++ src/App.js
@@ -12,28 +12,39 @@ class App extends React.Component {
     super(props);
     this.state = {
       searchQuery: '',
+      error: {},
+      isError: false,
       location: {},
-      weather: {}
+      weather: {},
     }
   }
 
   getLocation = async () => {
+    try {
+      const apiUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
 
-    const apiUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+      const response = await axios.get(apiUrl);
 
-    const response = await axios.get(apiUrl);
+      const backUrl = `http://localhost:3001/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}`;
 
-    const backUrl = `http://localhost:3001/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}`;
+      const weatherResponse = await axios.get(backUrl);
 
-    const weatherResponse = await axios.get(backUrl);
+      const location = response.data[0];
+      this.setState({
+        location: location,
+        weather: weatherResponse.data[0],
+        isError: false,
+      })
 
-    const location = response.data[0];
-
-    this.setState({
-      location: location, weather: weatherResponse.data[0]
-    });
-
+    } catch (error) {
+      this.setState({
+        error,
+        isError: true,
+        location: "",
+      });
+    }
   }
+
 
   render() {
     const img = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&format=jpg&zoom=12`;
@@ -45,15 +56,19 @@ class App extends React.Component {
           <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
           <Button onClick={this.getLocation}>Explore!</Button>
         </Navbar>
-
+       
+        {this.state.isError &&
+          <h1> ERROR! {this.state.error.message} </h1>
+          }
+        
         {this.state.location.place_id &&
           <>
             <h2>The city is: {this.state.location.display_name}</h2>
             <h3>The lattitude is: {this.state.location.lat}</h3>
             <h3>The longitude is: {this.state.location.lon}</h3>
-            <Weather 
-            date={this.state.weather.date}
-            description={this.state.weather.description}/>
+            <Weather
+              date={this.state.weather.date}
+              description={this.state.weather.description} />
 
             <img src={img} alt="location" id='map' />
 
